74c558e16a722c573ad2c0ef7c2e64e9a8045e1c
Shru-ti-00 <shrutimunda@gmail.com>
2026-04-30 23:58:39 +0530

    Updated code

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 
-int main()
+int BinarySearch(int arr[],int item)
 {
-    int arr[]={12,20,45,67,22,98,30};
-    int size=sizeof(arr)/sizeof(arr[0]);
+    
+    int size;
     int low=0,high=size-1,mid;
-    int item;
-    printf("Enter the element to be searched:");
-    scanf("%d",&item);
+    
+
+    if(low>high)
+        return -1;
 
     while(low<=high)
     {
@@ -21,6 +22,22 @@ int main()
             high=mid-1;
     }
 
-    return -1;
+}
 
+int main()
+{
+    int arr[]={12,20,45,67,22,98,30};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    int item;
+    printf("Enter the element to be searched:");
+    scanf("%d",&item);
+    
+    int result=BinarySearch(arr,item);
+
+    if(result!=-1)
+        printf("Element found at index %d ",result);
+    else
+        printf("Element not found");
+
+    return 0;
 }
