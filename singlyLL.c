745ef9ccc56aa122f35ca366da3ca0f26d0592a5
Shru-ti-00 <shrutimunda085@gmail.com>
2026-05-05 23:06:17 +0530

    Added comments

--- singlyLL.c
+++ singlyLL.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//structure of a node
 struct Node
 {
     int data;
     struct Node* next;
 };
 
+//creating node
 struct Node* createNode(int value)
 {
     struct Node* newNode=(struct Node*)malloc(sizeof(struct Node));
